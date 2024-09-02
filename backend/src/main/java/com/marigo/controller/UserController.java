d41b96b6971dd107f9428616b3b4d9d90d7c9198
marrigou <marrigo@163.com>
2024-09-02 13:51:00 +0800

    2.5 后端 - 用户管理

--- backend/src/main/java/com/marigo/controller/UserController.java
+++ backend/src/main/java/com/marigo/controller/UserController.java
@@ -3,13 +3,18 @@ import com.marigo.model.request.UserLoginRequest;
 import com.marigo.model.request.UserRegisterRequest;
 import com.marigo.service.UserService;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
+import static com.marigo.constant.UserConstant.ADMIN_ROLE;
+import static com.marigo.constant.UserConstant.USER_LOGIN_STATE;
 
 @RestController
 @RequestMapping("/user")
@@ -46,4 +51,46 @@ public class UserController {
         }
         return userService.userLogin(userAccount, userPassword, request);
     }
+
+
+    // 用户搜索
+    @GetMapping("/search")
+    public List<User> searchUser(String username, HttpServletRequest request){
+        if(!isAdmin(request)){
+            return new ArrayList<>();
+        }
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        if(StringUtils.isNotBlank(username)){
+            queryWrapper.like("username", username);
+        }
+        List<User> userList = userService.list(queryWrapper);
+        return userList.stream().map(user -> {
+            return userService.getSafetyUser(user);
+        }).collect(Collectors.toList());
+    }
+
+    // 用户删除
+    @PostMapping("/delete")
+    public boolean deleteUser(@RequestParam Long id, HttpServletRequest request){
+        // 管理员权限
+        if (!isAdmin(request)) {
+            return false;
+        }
+        return userService.removeById(id);
+    }
+
+
+    /**
+     * 是否为管理员
+     *
+     * @param request
+     * @return
+     */
+    private boolean isAdmin(HttpServletRequest request) {
+        // 仅管理员可查询
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User user = (User) userObj;
+        return user != null && user.getUserRole() == ADMIN_ROLE;
+    }
+
 }
